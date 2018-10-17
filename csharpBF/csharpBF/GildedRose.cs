27c93da3223f65a55b67c5891b78cb281512c4d1
FEZEUB <44173457+FEZEUB@users.noreply.github.com>
2018-10-17 18:04:44 +0200

    Add files via upload

---
+++ csharpBF/csharpBF/GildedRose.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                    switch (Items[i].Name)
+                    {
+                        case "Sulfuras, Hand of Ragnaros":
+                            break;
+
+                        case "Aged Brie":
+                            Items[i].SellIn--;
+                            if (Items[i].Quality < 50)
+                            {
+                                if (Items[i].SellIn < 0)
+                                    Items[i].Quality += 2;
+                                else
+                                    Items[i].Quality++;
+                            }
+                            break;
+
+                        case "Backstage passes to a TAFKAL80ETC concert":
+                            Items[i].SellIn--;
+                            if (Items[i].SellIn < 10 && Items[i].Quality < 50)
+                                Items[i].Quality ++;
+                            if (Items[i].SellIn < 5 && Items[i].Quality < 50)
+                                Items[i].Quality ++;
+
+                            if (Items[i].Quality < 50)
+                                Items[i].Quality++;
+                            if (Items[i].SellIn < 0)
+                                Items[i].Quality = 0;
+                            break;
+
+                        case "Conjured Mana Cake":
+                            if (Items[i].Quality > 0)
+                                Items[i].Quality -= 2;
+                                Items[i].SellIn--;
+                            break;
+
+                        default:
+                            Items[i].SellIn--;
+                            if (Items[i].Quality > 0 && Items[i].Quality < 50)
+                            {
+                                if (Items[i].SellIn < 0)
+                                        Items[i].Quality -= 2;
+                                else  
+                                        Items[i].Quality--;
+                            }
+                            break;
+                    }
+                
+            }
+        }
+    }
+    
+}
